6561ba41605d0aa26682effc7fadfaeb9fa9f277
DennekJr <54910517+DennekJr@users.noreply.github.com>
2022-05-22 16:08:49 -0500

    create ticket with project dropdown

--- BugTicketingSystemV2/Controllers/TicketsController.cs
+++ BugTicketingSystemV2/Controllers/TicketsController.cs
@@ -1,30 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BugTicketingSystemV2.Data;
 using BugTicketingSystemV2.Models;
 using Microsoft.AspNetCore.Identity;
-using BugTicketingSystemV2.Data.BLL;
-using System.Collections.Specialized;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BugTicketingSystemV2.Controllers
 {
+    [Authorize]
     public class TicketsController : Controller
     {
         private readonly BugTicketingSystemV2Context _context;
         private readonly UserManager<AppUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-
-        public ICollection<TicketHistory> TicketHistories { get; set; }
-        public ICollection<TicketComment> TicketComments { get; set; }
-        public ICollection<TicketAttachment> TicketAttachments { get; set; }
-        public ICollection<TicketNotification> TicketNotifications { get; set; }
-        public string SubmitterId;
-        public string UserId;
         public TicketRepository repo;
 
         public TicketsController(BugTicketingSystemV2Context context, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
@@ -37,9 +26,34 @@ namespace BugTicketingSystemV2.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //var bugTicketingSystemV2Context = _context.Tickets.Include(t => t.Submitter).Include(t => t.User);
-            ViewBag.SUsers = _userManager.Users.ToList();
+            string name = User.Identity.Name;
+            AppUser user = await _userManager.FindByEmailAsync(name);
+            Task<bool> IsSubmitter = _userManager.IsInRoleAsync(user, "Submitter");
+            Task<bool> IsDeveloperUser = _userManager.IsInRoleAsync(user, "Developer");
+            Task<bool> IsProjectManager = _userManager.IsInRoleAsync(user, "Project Manager");
+            //ViewBag.SUsers = await _userManager.Users.ToListAsync();
+            if (await IsSubmitter)
+            {
+                return View(repo.SubmitterTickets(user.Id));
+            } else if(await IsDeveloperUser)
+            {
+                return View(repo.DeveloperAssignedTickets(user.Id));
+            }
             return View(repo.GetAll());
+        }
+
+        [Authorize(Roles = "Submitter")]
+        public async Task<IActionResult> SubmitterTickets()
+        {
+            string name = User.Identity.Name;
+            SubmitterUser user = (SubmitterUser)await _userManager.FindByEmailAsync(name);
+            if(user == null)
+            {
+                return NotFound();
+            }
+            ViewBag.SUsers = _userManager.Users.ToList();
+
+            return View(repo.SubmitterTickets(user.Id));
         }
 
         public async Task<IActionResult> Details(int? id)
@@ -61,41 +75,46 @@ namespace BugTicketingSystemV2.Controllers
             return View(ticket);
         }
 
+        [Authorize(Roles = "Submitter")]
         public IActionResult Create()
         {
 
             ViewBag.YourEnumsStatus = new SelectList(Enum.GetValues(typeof(TicketStatus)), TicketStatus.Unassigned);
             ViewBag.YourEnumsType = new SelectList(Enum.GetValues(typeof(TicketType)), TicketType.InformationRequest);
             ViewBag.YourEnumsPriority = new SelectList(Enum.GetValues(typeof(TicketPriority)), TicketPriority.Low);
-
+            //Lis
+            //ViewBag.YourProjects = new SelectList(_context.Projects, "Pick a project");
+            ViewBag.YourProjects = _context.Projects.ToList();
             // Suggestion: should we add a default enum when creating
-
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,CreatedDate,ticketStatus,ticketType,ticketPriority")] Ticket ticket)
+        public async Task<IActionResult> Create(int projectId, [Bind("Id,Title,Description,CreatedDate,ticketStatus,ticketType,ticketPriority")] Ticket ticket)
         {
             string name = User.Identity.Name;
             SubmitterUser user = (SubmitterUser)await _userManager.FindByEmailAsync(name);
             ticket.SubmitterId = user.Id;
             ticket.Submitter = user;
+            Project project = _context.Projects.FirstOrDefault(x => x.Id == projectId);
             if(user != null)
             {
                 ViewBag.YourEnumsStatus = new SelectList(Enum.GetValues(typeof(TicketStatus)), ticket.ticketStatus);
                 ViewBag.YourEnumsType = new SelectList(Enum.GetValues(typeof(TicketType)), ticket.ticketType);
                 ViewBag.YourEnumsPriority = new SelectList(Enum.GetValues(typeof(TicketPriority)), ticket.ticketPriority);
-                var rand = new Random();
-                int num = rand.Next(1, 4);
-                ticket.Project = await _context.Projects.FindAsync(num);
-                _context.Add(ticket);
+                ViewBag.YourProjects = new SelectList(_context.Projects, "Pick a project");
+                //var rand = new Random();
+                //int num = rand.Next(1, 4);
+                ticket.Project = project;
+                _context.Tickets.Add(ticket);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(ticket);
         }
 
+        [Authorize(Roles="Developer")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.Tickets == null)
@@ -107,12 +126,6 @@ namespace BugTicketingSystemV2.Controllers
             {
                 return NotFound();
             }
-            TicketHistories = ticket.TicketHistories;
-            TicketComments = ticket.TicketComments;
-            TicketAttachments = ticket.TicketAttachments;
-            SubmitterId = ticket.SubmitterId;
-            UserId = ticket.UserId;
-            TicketNotifications = ticket.TicketNotifications;
             ViewBag.YourEnumsStatus = new SelectList(Enum.GetValues(typeof(TicketStatus)), ticket.ticketStatus);
             ViewBag.YourEnumsType = new SelectList(Enum.GetValues(typeof(TicketType)), ticket.ticketType);
             ViewBag.YourEnumsPriority = new SelectList(Enum.GetValues(typeof(TicketPriority)), ticket.ticketPriority);
@@ -137,15 +150,7 @@ namespace BugTicketingSystemV2.Controllers
             {
                 try
                 {
-                ticket.Title = Title;
-                ticket.Description = Description;
-                ticket.CreatedDate = CreatedDate;
-                ticket.UpdatedDate = UpdatedDate;
-                ticket.ticketStatus = ticketStatus;
-                ticket.ticketType = ticketType;
-                ticket.ticketPriority = ticketPriority;
-                repo.Update(ticket);
-                _context.SaveChangesAsync();
+                    repo.Edit(ticket, Title, Description, CreatedDate, UpdatedDate, ticketStatus, ticketType, ticketPriority);
             }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -163,6 +168,7 @@ namespace BugTicketingSystemV2.Controllers
             return View(ticket);
         }
 
+        [Authorize(Roles ="Project Manager")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Tickets == null)
@@ -193,7 +199,8 @@ namespace BugTicketingSystemV2.Controllers
             var ticket = await _context.Tickets.FindAsync(id);
             if (ticket != null)
             {
-                _context.Tickets.Remove(ticket);
+                repo.Remove(ticket);
+                repo.Save();
             }
             
             await _context.SaveChangesAsync();
--- BugTicketingSystemV2/Data/TicketRepository.cs
+++ BugTicketingSystemV2/Data/TicketRepository.cs
@@ -38,9 +38,51 @@ namespace BugTicketingSystemV2.Data
 			return allTickets.ToList();
 		}
 
+		public ICollection<Ticket> SubmitterTickets(string submitterId)
+        {
+			List<Ticket> Tickets = context.Tickets.Where(s => s.SubmitterId == submitterId).ToList();
+			return Tickets;
+        }
+
+		public ICollection<Ticket> DeveloperAssignedTickets(string devID)
+		{
+			List<Ticket> Tickets = context.Tickets.Where(s => s.UserId == devID).ToList();
+			return Tickets;
+		}
+
+		//public ICollection<Ticket> ProjectTickets(string projectManagerId)
+		//{
+		//	List<Ticket> ProjectManagerTickets = new List<Ticket>();
+		//	foreach(var project in context.Projects)
+  //          {
+		//		foreach(var ticket in project.Tickets)
+  //              {
+		//			if()
+  //              }
+  //          }
+		//}
+
 		public ICollection<Ticket> GetList(Func<Ticket, bool> whereFunction)
 		{
 			return context.Tickets.Where(whereFunction).ToList();
+		}
+
+		public void Create()
+        {
+
+        }
+
+		public void Edit(Ticket ticket, string Title, string Description, DateTime CreatedDate, DateTime UpdatedDate, TicketStatus ticketStatus, TicketType ticketType, TicketPriority ticketPriority)
+        {
+			ticket.Title = Title;
+			ticket.Description = Description;
+			ticket.CreatedDate = CreatedDate;
+			ticket.UpdatedDate = UpdatedDate;
+			ticket.ticketStatus = ticketStatus;
+			ticket.ticketType = ticketType;
+			ticket.ticketPriority = ticketPriority;
+			context.Update(ticket);
+			context.SaveChangesAsync();
 		}
 
 		public void Update(Ticket ticket)
