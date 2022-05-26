8efc8b90a63eb9587e96ab85bfb57f1a9175c1e9
raminelnunez <Raminelnc23@gmail.com>
2022-05-26 16:33:40 -0500

    commenting stuff out on rolebusinesslogic

--- BugTicketingSystemV2/Data/BLL/RoleBusinessLogic.cs
+++ BugTicketingSystemV2/Data/BLL/RoleBusinessLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using BugTicketingSystemV2.Data.DAL;
 using BugTicketingSystemV2.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTicketingSystemV2.Data.BLL
@@ -10,13 +11,11 @@ namespace BugTicketingSystemV2.Data.BLL
 		private RoleManager<IdentityRole> roleManager;
 		private UserManager<AppUser> userManager;
 
-		public RoleController(RoleManager<IdentityRole> roleMgr, UserManager<AppUser> userMrg)
-		{
-			roleManager = roleMgr;
-			userManager = userMrg;
-		}
-
-		
+		//public RoleController(RoleManager<IdentityRole> roleMgr, UserManager<AppUser> userMrg)
+		//{
+		//	roleManager = roleMgr;
+		//	userManager = userMrg;
+		//}
 
 
 	}
